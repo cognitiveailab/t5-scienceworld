48e5a73b63ee47918bffbe365801e3a0906b4d04
Ruoyao Wang <wsxzwps43@126.com>
2022-06-03 23:45:35 -0700

    update weight downloading address and scienceworld version

--- download_pretrained_model.sh
+++ download_pretrained_model.sh
@@ -1,5 +1,5 @@
-gsutil cp -r gs://ai2-raja-public/scienceworld/sciworld_models/11b_sciworld_$1_hf .
-gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/sciworld_3b_$1/special_tokens_map.json 11b_sciworld_$1_hf
-gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/sciworld_3b_$1/spiece.model 11b_sciworld_$1_hf
-gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/sciworld_3b_$1/tokenizer.json 11b_sciworld_$1_hf
-gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/sciworld_3b_$1/tokenizer_config.json 11b_sciworld_$1_hf
+gsutil cp -r gs://ai2-raja-public/scienceworld/sciworld_models/rerun/sciworld_11b_rerun_$1 .
+gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/rerun/sciworld_3b_rerun_$1/special_tokens_map.json sciworld_11b_rerun_$1
+gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/rerun/sciworld_3b_rerun_$1/spiece.model sciworld_11b_rerun_$1
+gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/rerun/sciworld_3b_rerun_$1/tokenizer.json sciworld_11b_rerun_$1
+gsutil cp gs://ai2-raja-public/scienceworld/sciworld_models/rerun/sciworld_3b_rerun_$1/tokenizer_config.json sciworld_11b_rerun_$1
